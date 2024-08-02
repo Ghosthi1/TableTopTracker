eed619c5a18af561cb70cd38e561a52fd3d670a8
Callum Forsyth <c-forsyth02@sky.com>
2024-08-02 15:21:00 +0100

    Added a Tag checker

--- My project/Assets/Buttons.cs
+++ My project/Assets/Buttons.cs
@@ -5,30 +5,50 @@ using UnityEngine.EventSystems;
 using TMPro;
 using System;
 using System.Diagnostics.Tracing;
+using UnityEngine.UIElements;
 
 public class Buttons : MonoBehaviour
 {
-    public int counter = 0;
+    private int counter = 1;
+
+    private void Buttonpressed(TextMeshProUGUI currentText){
+            //main manager
+            LimitChecker(currentText);
+            
+    }
     public void AddButton(TextMeshProUGUI currentText){
             counter += 1;
             currentText.text = counter.ToString();
-            LimitChecker(currentText);
+            Buttonpressed(currentText);
     }
 
     public void MinusButton(TextMeshProUGUI currentText){
             counter -= 1;
             currentText.text = counter.ToString();
-            LimitChecker(currentText);
+            Buttonpressed(currentText);
     }
 
     private void LimitChecker(TextMeshProUGUI currentText){
             //checks the bounds of the values 
             //gets string value 
-            String currentnumber = currentText.ToString();
-            //converts to int 
-            int.TryParse(currentnumber , out int intNumber);
-            if(intNumber < 0 ){
-                currentText.text = 0.ToString();
+            String currentnumber = currentText.text; 
+            //converts to a int and compares 
+            if(int.Parse(currentnumber) < 0){
+                counter = 0;
+                currentText.text = "0";
+                Debug.Log("Points to low"); 
             }
     }
+
+    public void TagChecker(GameObject button){
+        //checks the tag to appropriatly deal with buttons 
+        
+        if(button.CompareTag("Plus")){
+            Debug.Log("Plus"); 
+        }
+        if(button.CompareTag("Negative")){
+            Debug.Log("Minus"); 
+        }
+    }
+
 }
