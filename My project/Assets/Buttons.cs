2af99d90fdefec3a32a1f21a0272a663d73b98a1
Callum Forsyth <c-forsyth02@sky.com>
2024-08-04 00:49:08 +0100

    Added all tagger checker

--- My project/Assets/Buttons.cs
+++ My project/Assets/Buttons.cs
@@ -1,20 +1,20 @@
-using Unity.VisualScripting;
+
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using TMPro;
-using System;
-using System.Diagnostics.Tracing;
-using UnityEngine.UIElements;
-using System.Runtime.Serialization.Json;
+
 
 public class Buttons : MonoBehaviour
 {
-
+    public GameObject totalP1;
+    public GameObject totalP2;
     private int round = 1;
-    public int turn = 1;
-    public int cp1  = 1;
+    private int turn = 1;
+    private int cp1  = 1;
     private int cp2  = 1;
+    private int primary1 = 0;
+    private int primary2 = 0;
+    private int secondary1 = 0;
+    private int secondary2 = 0;
 
     //plus for true neg for false 
     private bool whatbuttonPressed;
@@ -29,35 +29,39 @@ public class Buttons : MonoBehaviour
         }
     }
 
-    private void AddButton(TextMeshProUGUI currentText, ref int correctCounter){
-            correctCounter ++;
+    private int AddButton(TextMeshProUGUI currentText, int counter){
+            counter ++;
             //coverts counter to sttring and updates game 
-            currentText.text = correctCounter.ToString();
+            currentText.text = counter.ToString();
+            return counter;
     }
 
-    public void MinusButton(TextMeshProUGUI currentText, ref int correctCounter){
-            correctCounter --;
-            currentText.text = correctCounter.ToString();
+    public int MinusButton(TextMeshProUGUI currentText, int counter){
+            counter --;
+            currentText.text = counter.ToString();
+            return counter;
     }
 
-    private bool LimitChecker(TextMeshProUGUI currentText,int minValue, int maxValue, ref int correctCounter ){
+    private int LimitChecker(TextMeshProUGUI currentText,int minValue, int maxValue, int counter ){
         //retuns bool to show upper bound "true" or lower bound "false"
             //checks the bounds of the values   
             //Checks if Greater than or less than bounds
-            if( correctCounter < minValue){
-                correctCounter = minValue;
+
+            if( counter < minValue){
+                counter = minValue;
                 currentText.text = minValue.ToString();
                 Debug.Log("Points to low"); 
-                return false;
+                return counter;
             }
-            if(correctCounter > maxValue){
-                correctCounter = minValue;
+            if(counter > maxValue){
+                counter = minValue;
                 currentText.text = minValue.ToString();
                 Debug.Log("Points to High"); 
-                return true;
+                return counter;
             }
             //default return 
-            return false;
+            Debug.Log("Default"); 
+            return counter;
            
     }
 
@@ -71,13 +75,12 @@ public class Buttons : MonoBehaviour
 
             //deals with what button was pressed
             if(whatbuttonPressed){
-                AddButton(ObjectText, ref turn);
+                turn = AddButton(ObjectText, turn);            
             }
             if(!whatbuttonPressed){
-                MinusButton(ObjectText, ref turn);
+                turn = MinusButton(ObjectText, turn);
             }
-
-            if(LimitChecker(ObjectText, 1,2 , ref turn)){
+            if(turn > 2){
             //if higher than max value 
                 round++;
                 if(round > 5)
@@ -93,30 +96,124 @@ public class Buttons : MonoBehaviour
                     //sets the text 
                     roundsText.text = "Round: " + round.ToString();
                 }
-
             }
+
+            turn = LimitChecker(ObjectText, 1,2 , turn);
+
         }
 
         if(Object.CompareTag("CP1")){
-        //counter used here is cpCount1 
+        //counter used here is cp1 
 
             //gets the text 
             TextMeshProUGUI ObjectText = Object.GetComponent<TextMeshProUGUI>();
             //deals with what button was pressed
             if(whatbuttonPressed){
-                AddButton(ObjectText, ref turn);
+                cp1 = AddButton(ObjectText, cp1);       
             }
             if(!whatbuttonPressed){
-                MinusButton(ObjectText, ref turn);
+                cp1 = MinusButton(ObjectText, cp1); 
             }
 
-            if(LimitChecker(ObjectText,0,5, ref cp1)){
-
-            }
+            cp1 = LimitChecker(ObjectText,0,5, cp1);  
         }
         
+        if(Object.CompareTag("CP2")){
+        //counter used here is cp2 
 
+            //gets the text 
+            TextMeshProUGUI ObjectText = Object.GetComponent<TextMeshProUGUI>();
+            //deals with what button was pressed
+            if(whatbuttonPressed){
+                cp2 = AddButton(ObjectText, cp2);       
+            }
+            if(!whatbuttonPressed){
+                cp2 = MinusButton(ObjectText, cp2); 
+            }
 
+            cp2 = LimitChecker(ObjectText,0,5, cp2);  
+        }
+
+        if(Object.CompareTag("Primary1")){
+        //counter used here is primary1
+
+            //gets the text 
+            TextMeshProUGUI ObjectText = Object.GetComponent<TextMeshProUGUI>();
+            //deals with what button was pressed
+            if(whatbuttonPressed){
+                primary1 = AddButton(ObjectText, primary1);       
+            }
+            if(!whatbuttonPressed){
+                primary1 = MinusButton(ObjectText, primary1); 
+            }
+
+            primary1 = LimitChecker(ObjectText,0,100, primary1);  
+            UpdateTotal(1); 
+        }
+
+        if(Object.CompareTag("Primary2")){
+        //counter used here is primary2 
+
+            //gets the text 
+            TextMeshProUGUI ObjectText = Object.GetComponent<TextMeshProUGUI>();
+            //deals with what button was pressed
+            if(whatbuttonPressed){
+                primary2 = AddButton(ObjectText, primary2);       
+            }
+            if(!whatbuttonPressed){
+                primary2 = MinusButton(ObjectText, primary2); 
+            }
+
+            primary2 = LimitChecker(ObjectText,0,100, primary2); 
+            UpdateTotal(2); 
+        }
+        
+        if(Object.CompareTag("Secondary1")){
+        //counter used here is secondary1 
+            Debug.Log("Done");
+            //gets the text 
+            TextMeshProUGUI ObjectText = Object.GetComponent<TextMeshProUGUI>();
+            //deals with what button was pressed
+            if(whatbuttonPressed){
+                secondary1 = AddButton(ObjectText, secondary1);       
+            }
+            if(!whatbuttonPressed){
+                secondary1 = MinusButton(ObjectText, secondary1); 
+            }
+
+            secondary1 = LimitChecker(ObjectText,0,100, secondary1); 
+            UpdateTotal(1); 
+        }
+
+        if(Object.CompareTag("Secondary2")){
+        //counter used here is secondary2 
+
+            //gets the text 
+            TextMeshProUGUI ObjectText = Object.GetComponent<TextMeshProUGUI>();
+            //deals with what button was pressed
+            if(whatbuttonPressed){
+                secondary2 = AddButton(ObjectText, secondary2);       
+            }
+            if(!whatbuttonPressed){
+                secondary2 = MinusButton(ObjectText, secondary2); 
+            }
+
+            secondary2 = LimitChecker(ObjectText,0,100, secondary2);  
+            UpdateTotal(2);
+        }
+
+    }
+    
+    private void UpdateTotal(int player){
+        if(player == 1){
+            TextMeshProUGUI player1 = totalP1.GetComponent<TextMeshProUGUI>();
+            player1.text = (primary1 + secondary1).ToString();
+        }
+        if(player == 2){
+            TextMeshProUGUI player2 = totalP2.GetComponent<TextMeshProUGUI>();
+            player2.text = (primary2 + secondary2).ToString();
+        }
+ 
     }
 
 }
